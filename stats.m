9d660e6863b6ca8fe214984f831a4dc255849e48
lat160130 <80650990+lat160130@users.noreply.github.com>
2021-10-28 10:11:24 -0500

    Create stats.m
    This is our initial script.  It does nothing.  The beauty of git is that if you mess up, there will be copies of previous codes that (hopefully) worked well enough.

---
+++ stats.m
@@ -0,0 +1,6 @@
+% Here is the initial MATLAB script.
+% Right now without editing all it will do is say "Hello, World" because that is what K&R recommend.
+
+fprintf("Hello, World.\n");
+
+% Make sure you've gone through the tutorial.  Best of luck.
