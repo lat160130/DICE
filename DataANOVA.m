4dd3789b779650d27a876e0da58771e0ba518a7f
lat160130 <lat160130@tamu.edu>
2021-11-15 21:12:00 -0600

    adding files to repo to be edited

---
+++ DataANOVA.m
@@ -0,0 +1,10 @@
+function [results,means,stats,results1,means1,stats1] = DataANOVA(Serious_Threat_Sex0,Serious_Threat_Sex1)
+% Anova Test 1
+
+[p,tbl,stats] = anova1(Serious_Threat_Sex0)
+[results,means] = multcompare(stats)
+[p1,tbl1,stats1] = anova1(Serious_Threat_Sex1)
+[results1,means1] = multcompare(stats)
+
+end
+
---
+++ pull_from_excel.m
@@ -0,0 +1,25 @@
+xlsx_name = 'COVID_Questions_Plots_Sex_and_Lone.xlsx';
+Serious_Threat = readmatrix(xlsx_name,'Sheet','Serious Threat','Range','B2:L84');
+
+Serious_Threat_Sex0 = readmatrix(xlsx_name,'Sheet','Serious Threat Sex','Range','C2:M35');
+
+Serious_Threat_Sex1 = readmatrix(xlsx_name,'Sheet','Serious Threat Sex','Range','Q2:AA49');
+
+size_ST_Sex0 = size(Serious_Threat_Sex0);
+rows = size_ST_Sex0(1);
+cols = size_ST_Sex0(2);
+
+list_Serious_Threat_Sex0 = [];
+i = 1;
+j = 1;
+while i < rows+1
+    j = 1;
+  while j <  cols+1
+      list_Serious_Threat_Sex0 = [list_Serious_Threat_Sex0, Serious_Threat_Sex0(i, j)];
+      j= j+1;
+  end
+  i= i+1;
+end
+
+% 
+list_Serious_Threat_Sex0 = list_Serious_Threat_Sex0(~isnan(list_Serious_Threat_Sex0));
