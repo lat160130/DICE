ff443df9f0c2a05f157a7c49a2907f4d6cfc029d
lat160130 <lat160130@tamu.edu>
2021-11-15 21:54:59 -0600

    no new changes

--- test_readin.m
+++ test_readin.m
@@ -2,7 +2,10 @@ clc;
 clear;
 close all;
 
-xlsx_name = 'dice_cov_stim_fitness_sex_alone.xlsx';
-User_Que_MAT_dot = readmatrix(xlsx_name,'Sheet','User and Question Over Time - S','Range','B2:L84');
-[numbers, strings, raw] = xlsread(xlsx_name,'User and Question Over Time - S');
-t2 = readmatrix(xlsx_name,'Sheet','User and Question Over Time - A','Range','B2:L84');
+% xlsx_name = 'dice_cov_stim_fitness_sex_alone.xlsx';
+% User_Que_MAT_dot = readmatrix(xlsx_name,'Sheet','User and Question Over Time - S','Range','B2:L84');
+% [numbers, strings, raw] = xlsread(xlsx_name,'User and Question Over Time - S');
+% t2 = readmatrix(xlsx_name,'Sheet','User and Question Over Time - A','Range','B2:L84');
+
+
+[results,means,stats,results1,means1,stats1] = DataANOVA(Serious_Threat_Sex0,Serious_Threat_Sex1);
